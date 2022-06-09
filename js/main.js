223d7a9319624c9f5f5a392b1b0d564db7cd2fc7
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-09 16:18:46 +0200

    added changes html

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ new Vue(
         el: '#root',
         data: {
             messaggio: "Welcome in Vue Js",
-        }
+        },
+            immagine :['https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg',
+            'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg']
     }
 );
